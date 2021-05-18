46e8d2f80ac18b90bb787e9e69e5a9f334f0a022
shuaiquan <994011557@qq.com>
2021-05-18 08:42:18 +0800

    调整接口

--- packages/math/src/matrix3/index.ts
+++ packages/math/src/matrix3/index.ts
@@ -39,24 +39,6 @@ class Matrix3 {
     }
 
     /**
-     * Returns the transpose of the matrix
-     * 
-     * todo 待定
-     */
-    static transposeMatrix(matrix: Matrix3) {
-        return matrix.clone().transpose();
-    }
-
-    /**
-     * Returns the inverse of the matrix
-     * 
-     * todo 待定
-     */
-    static invertMatrix(matrix: Matrix3) {
-        return matrix.clone().invert();
-    }
-
-    /**
      * @returns the array result ma x mb
      */
     private static product(a: Matrix3, b: Matrix3) {
@@ -162,6 +144,52 @@ class Matrix3 {
     }
 
     /**
+     * Sets this matrix from translate transform
+     * @returns The current matrix
+     */
+    fromTranslate(v: Vector2) {
+        this.set(
+            1, 0, v.x,
+            0, 1, v.y,
+            0, 0, 1,
+        );
+
+        return this;
+    }
+
+    /**
+     * Sets this matrix from scale transform
+     * @returns The current matrix
+     */
+    fromScale(v: Vector2) {
+        this.set(
+            v.x, 0, 0,
+            0, v.y, 0,
+            0, 0, 1,
+        )
+
+        return this;
+    }
+
+    /**
+     * Sets this matrix from rotate transform
+     * @param radian theta Rotation angle in radians.
+     * @returns The current matrix
+     */
+    fromRotate(radian: number) {
+        const c = Math.cos(radian);
+        const s = Math.sin(radian);
+
+        this.set(
+            c, -s, 0,
+            s, c, 0,
+            0, 0, 1,
+        );
+
+        return this;
+    }
+
+    /**
      * Copies m to the current matrix
      * @returns The current matrix
      */
@@ -209,27 +237,26 @@ class Matrix3 {
     }
 
     /**
-     * Transposes this matrix
-     * 
-     * todo new matrix
+     * Transposes this matrix (转置矩阵)
+     * @returns A new matrix
      */
     transpose() {
+        const arr: number[] = [];
+
         const te = this.elements;
-        let tmp = 0;
+        arr[0] = te[0]; arr[1] = te[3]; arr[2] = te[6];
+        arr[3] = te[1]; arr[4] = te[4]; arr[5] = te[7];
+        arr[6] = te[2]; arr[7] = te[5]; arr[8] = te[8];
 
-        tmp = te[1]; te[1] = te[3]; te[3] = tmp;
-        tmp = te[2]; te[2] = te[6]; te[6] = tmp;
-        tmp = te[5]; te[5] = te[7]; te[7] = tmp;
-
-        return this;
+        return new Matrix3().fromArray(arr);
     }
 
     /**
-     * Inverts this matrix.
-     * 
-     * todo new matrix
+     * Inverts this matrix （逆矩阵）
+     * @returns A new matrix
      */
     invert() {
+        // todo 整理一下算法
         const [
             m11, m12, m13,
             m21, m22, m23,
@@ -248,13 +275,11 @@ class Matrix3 {
         const n32 = -(m11 * m32 - m31 * m12) * d;
         const n33 = (m11 * m22 - m21 * m12) * d;
 
-        this.set(
+        return new Matrix3(
             n11, n12, n13,
             n21, n22, n23,
             n31, n32, n33,
         );
-
-        return this;
     }
 
     /**
@@ -274,41 +299,36 @@ class Matrix3 {
 
     /**
      * Applies translate transform to this matrix
-     * 
-     * todo new matrix
+     * @returns A new matrix
      */
     applyTranslate(v: Vector2) {
-        const te = this.elements;
+        const arr = [...this.elements];
+        arr[2] += v.x;
+        arr[5] += v.y;
 
-        te[2] += v.x;
-        te[5] += v.y;
-
-        return this;
+        return new Matrix3().fromArray(arr);
     }
 
     /**
      * Applies scale transform to this matrix
-     * 
-     * todo new matrix
+     * @returns A new matrix
      */
     applyScale(v: Vector2) {
-        const te = this.elements;
+        const arr = [...this.elements];
+        arr[0] *= v.x;
+        arr[1] *= v.x;
+        arr[2] *= v.x;
+        arr[3] *= v.y;
+        arr[4] *= v.y;
+        arr[5] *= v.y;
 
-        te[0] *= v.x;
-        te[1] *= v.x;
-        te[2] *= v.x;
-        te[3] *= v.y;
-        te[4] *= v.y;
-        te[5] *= v.y;
-
-        return this;
+        return new Matrix3().fromArray(arr);
     }
 
     /**
      * Applies rotate transform to this matrix
      * @param radian theta Rotation angle in radians.
-     * 
-     * todo new matrix
+     * @returns A new matrix
      */
     applyRotate(radian: number) {
         const [
@@ -320,59 +340,18 @@ class Matrix3 {
         const c = Math.cos(radian);
         const s = Math.sin(radian);
 
-        const te = this.elements;
+        const arr: number[] = [];
+        arr[0] = m11 * c - m21 * s;
+        arr[1] = m12 * c - m22 * s;
+        arr[2] = m13 * c - m23 * s;
+        arr[3] = m11 * s + m21 * c;
+        arr[4] = m12 * s + m22 * c;
+        arr[5] = m13 * s + m23 * c;
+        arr[6] = m31;
+        arr[7] = m32;
+        arr[8] = m33;
 
-        te[0] = m11 * c - m21 * s;
-        te[1] = m12 * c - m22 * s;
-        te[2] = m13 * c - m23 * s;
-        te[3] = m11 * s + m21 * c;
-        te[4] = m12 * s + m22 * c;
-        te[5] = m13 * s + m23 * c;
-
-        return this;
-    }
-
-    /**
-     * Sets this matrix from translate transform
-     */
-    fromTranslate(v: Vector2) {
-        this.set(
-            1, 0, v.x,
-            0, 1, v.y,
-            0, 0, 1,
-        );
-
-        return this;
-    }
-
-    /**
-     * Sets this matrix from scale transform
-     */
-    fromScale(v: Vector2) {
-        this.set(
-            v.x, 0, 0,
-            0, v.y, 0,
-            0, 0, 1,
-        )
-
-        return this;
-    }
-
-    /**
-     * Sets this matrix from rotate transform
-     * @param radian theta Rotation angle in radians.
-     */
-    fromRotate(radian: number) {
-        const c = Math.cos(radian);
-        const s = Math.sin(radian);
-
-        this.set(
-            c, -s, 0,
-            s, c, 0,
-            0, 0, 1,
-        );
-
-        return this;
+        return new Matrix3().fromArray(arr);
     }
 
     /**
