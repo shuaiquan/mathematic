e169962b4fa82d3885f7a3669135fed5e51dc1c1
shuaiquan <994011557@qq.com>
2021-05-22 23:40:24 +0800

    扩展 NumberUtil

--- packages/math/src/utils/index.ts
+++ packages/math/src/utils/index.ts
@@ -4,6 +4,22 @@ class NumberUtil {
     static isEqual(v1: number, v2: number, tolerance = TOLERANCE) {
         return Math.abs(v1 - v2) < tolerance;
     }
+
+    /**
+     * 获取相反数
+     */
+    static getOppositeNumber(value: number) {
+        return value === 0 ? 0 : -value;
+    }
+
+    /**
+     * 将有符号的 Zero 转化为无符号
+     * 
+     * Object.is vs === : https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Object/is
+     */
+    static unSignedZero(value: number) {
+        return value === 0 ? 0 : value;
+    }
 }
 
 export {
