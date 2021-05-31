0e703a57bdc857f1542f287ec4e03b21b354013a
shuaiquan <994011557@qq.com>
2021-06-01 03:08:39 +0800

    导出 line2

--- packages/math/src/index.ts
+++ packages/math/src/index.ts
@@ -2,3 +2,4 @@ export { Vector2, IVec2 } from './unit/vector2';
 export { Vector3, IVec3 } from './unit/vector3';
 export { Matrix3 } from './unit/matrix3';
 export { Matrix4 } from './unit/matrix4';
+export { Line2, LineSide } from './unit/line2';
