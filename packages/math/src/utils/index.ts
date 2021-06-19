911b9ecee31b81f25745110ac49d3fd716eb6ae5
shuaiquan <994011557@qq.com>
2021-06-19 19:58:04 +0800

    util 增加 vector3 方法

--- packages/math/src/utils/index.ts
+++ packages/math/src/utils/index.ts
@@ -1,9 +1,11 @@
 import { Line2Util } from "./line2";
 import { NumberUtil } from "./number";
-import { Vector2Util } from "./Vector2";
+import { Vector2Util } from "./vector2";
+import { Vector3Util } from './vector3';
 
 export const Utils = {
     Number: NumberUtil,
     Vector2: Vector2Util,
+    Vector3: Vector3Util,
     Line2: Line2Util,
 };
---
+++ packages/math/src/utils/vector3.ts
@@ -0,0 +1,30 @@
+import { Vector3 } from "../unit/vector3";
+
+class Vector3Util {
+    /**
+     * Computes cross product of (v2 -> v1) and (v3 -> v1)
+     */
+    static cross3(v1: Vector3, v2: Vector3, v3: Vector3) {
+        const v12 = v2.sub(v1);
+        const v13 = v3.sub(v1);
+        return v12.cross(v13);
+    }
+
+    /**
+     * Computes dot product of (v2 -> v1) and (v3 -> v1)
+     */
+    dot3(v1: Vector3, v2: Vector3, v3: Vector3) {
+        const v12 = v2.sub(v1);
+        const v13 = v3.sub(v1);
+        return v12.dot(v13);
+    }
+
+    /**
+     * Computed the distance from v1 to v2
+     */
+    distance(v1: Vector3, v2: Vector3) {
+        return v2.sub(v1).length;
+    }
+}
+
+export { Vector3Util };
