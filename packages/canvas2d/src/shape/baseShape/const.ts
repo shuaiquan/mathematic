65849d72c847b88dfb0457e9b95838fb9d560c19
shuaiquan <994011557@qq.com>
2021-10-01 10:43:16 +0800

    标准化图形接口

--- packages/canvas2d/src/shape/baseShape/const.ts
+++ packages/canvas2d/src/shape/baseShape/const.ts
@@ -1,10 +1,14 @@
 import { LineCap, LineJoin, StyleOption } from "./type";
 
+/**
+ * 默认样式配置
+ */
 export const DEFAULT_STYLE_OPTION: StyleOption = {
-    fillColor: '#000',
+    alpha: 1,
+    fillStyle: '#fff',
     fillAlpha: 1,
-    lineColor: '#000',
-    lineAlpha: 1,
+    strokeStyle: '#000',
+    strokeAlpha: 1,
     lineWidth: 1,
     lineCap: LineCap.Butt,
     lineJoin: LineJoin.Miter,
--- packages/canvas2d/src/shape/circle/shape.ts
+++ packages/canvas2d/src/shape/circle/shape.ts
@@ -1,31 +1,55 @@
 import { Vector2 } from "@s7n/math";
-import { BaseShape } from "../baseShape";
+import { BaseShape, PartialStyleOption } from "../baseShape";
 
 class Circle extends BaseShape {
+    /**
+     * 圆心坐标
+     */
     private center: Vector2 = new Vector2();
 
+    /**
+     * 圆半径
+     */
     private radius: number = 0;
 
-    constructor(center: Vector2, radius: number) {
-        super();
+    constructor(center: Vector2, radius: number, styleOption?: PartialStyleOption) {
+        super(styleOption);
         this.center = center.clone();
         this.radius = radius;
     }
 
+    /**
+     * 设置圆心坐标
+     */
     setCenter(center: Vector2) {
         this.center = center.clone();
     }
 
+    /**
+     * 获取圆心坐标
+     */
     getCenter() {
-        return this.center;
+        return this.center.clone();
     }
 
+    /**
+     * 设置半径
+     */
     setRadius(radius: number) {
         this.radius = radius;
     }
 
+    /**
+     * 获取半径
+     */
     getRadius() {
         return this.radius;
+    }
+
+    getPath() {
+        const path = new Path2D();
+        path.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2);
+        return path;
     }
 }
 
